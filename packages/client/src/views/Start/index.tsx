554ab14fc2707eaf11398d560acd96b2dbbacd8b
noxcho <bubaev.d@roistat.com>
2023-04-29 16:02:16 +0300

    create start page

--- packages/client/src/views/Start/index.tsx
+++ packages/client/src/views/Start/index.tsx
@@ -1,32 +1,24 @@
 
+import Header from '../../components/UI/Header'
+import Button from '../../components/UI/Button'
 
-import styles from './style.module.css'
+import styles from './style.module.scss'
 
 const Start = () => {
     return (
         <div className={styles['game-container']}>
             <div className={styles['content']}>
-                <div className="game-container_title">
-                    Blade Runner 
-                </div>
+                <Header title="BLADE RUNNER" />
 
                 <div className={styles['start-content']}>
-                    <button>
-                        Start Game
-                    </button>
-                    <button>
-                        Settings
-                    </button>
+                    <Button label="Start Game" />
+                    <Button label="Settings" />
                 </div>
             </div> 
             
             <div className={styles['buttons']}>
-                <button>
-                    Leaderboard
-                </button>
-                <button>
-                    Forum
-                </button>
+                <Button label="Leaderboard" />
+                <Button label="Forum" />
             </div>
         </div>
     );
