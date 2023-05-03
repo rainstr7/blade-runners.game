8f1fec9b6841f9d2707c2958a347abd06add9925
kgalkin <k1raGalkin@yandex.ru>
2023-05-03 20:50:39 +0400

    feat(landingpage): add files for landing page
    just start

---
+++ packages/client/src/views/Main/index.tsx
@@ -0,0 +1,15 @@
+const Main = () => {
+  return (
+    <main>
+      <header>
+        header
+      </header>
+      <div>MAIn</div>
+      <div>about</div>
+      <div>galery</div>
+      <footer>footer</footer>
+    </main>
+  )
+}
+
+export default Main;
