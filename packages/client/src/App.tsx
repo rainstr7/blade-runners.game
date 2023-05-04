e2ab3d4dd0f5f57e33f1cf179a480aaf24e2bcbc
OvalniyKeks <keksovalniy@gmail.com>
2023-05-04 21:29:24 +0300

    fix: 🐛 remove comments code

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-// import { Game } from './views/Game'
 import LeaderBoard from './views/Leaderboard'
 import SignUp from './views/SignUp'
 import Auth from './views/Auth'
