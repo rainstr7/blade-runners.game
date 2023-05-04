61508de9aefe3663a72a85518b1b19f78cab8800
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-05-04 16:39:51 +0300

    Add Forum
    add Forum pages and styles

---
+++ packages/client/src/components/Forum/ForumPage.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import styles from './ForumPage.module.scss'
+import { forums, topics } from './Forum'
+import Button from '../UI/Button';
+import { Topic, Forum } from './Forum';
+
+export const ForumPage: React.FC = () => {
+  const navigate = useNavigate();
+ 
+  function handleGoBack() {
+    navigate('/forum');
+  }
+  function handleGoCreate(){
+    navigate('/forum/newtheme')
+  }
+  const { id } = useParams<{ id: string }>();
+  const forum: Forum | undefined = forums.find((f) => f.id === id);
+  const { topics } = forum
+
+  if (!forum) {
+    // если форум с таким id не найден, отображаем страницу ошибки
+    return <div>Форум не найден</div>;
+  }
+
+  return (
+    <div>
+      <nav className={styles.TopicHeader}>
+        <Button size='small' onClick={handleGoBack}>Back</Button>
+        <h2>{forum.title}</h2>
+        <Button size='small' onClick={handleGoCreate}>CREATE THEME</Button>
+      </nav>
+      <ul className={styles.ListContainer}>
+      <li className={styles.ListElement}>
+            <div className={styles.HeaderContainer}>
+              <div className={styles.TitleHeader}>THEME</div>
+              <div className={styles.MsgCountHeader}>COMMENTS</div>
+            </div>
+          </li>
+        {topics.map((topic: Topic) => (
+          <li className={styles.ListElement} key={topic.id}>
+            <Link className={styles.LinkElement}  to={`/forum/${forum.id}/${topic.id}`}>
+            <div className={styles.Title}>{topic.title}</div>
+                <div className={styles.MsgCount}>{topic.messagesCount}</div>
+              </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ packages/client/src/components/Forum/ThemePage.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import {
+  useNavigate,
+  useParams,
+  useLocation,
+} from 'react-router-dom';
+import { nanoid } from 'nanoid'
+import styles from './ThemePage.module.scss';
+// import ava from '../../assets/ava.png'
+import { forums, messages, topics } from './Forum'
+import Button from '../UI/Button';
+
+export const ThemePage: React.FC = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  function removePathSuffix(path: string): string {
+    const index = path.lastIndexOf('/');
+    if (index >= 0) {
+      return path.substring(0, index);
+    } else {
+      return path;
+    }
+  }
+
+  function handleGoBack() {
+    navigate(removePathSuffix(pathname));
+  }
+  const { id } = useParams<{ id: string }>();
+  const topic = topics.find((f) => f.id === id);
+
+  if (!topic) {
+    // если форум с таким id не найден, отображаем страницу ошибки
+    return <div>Тема не найден</div>;
+  }
+
+  const [state, setState] = React.useState(messages)
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const time = `${hours}:${minutes}`; 
+    const messageInput = event.target.elements.message;
+    const message = messageInput.value;
+    messageInput.value = '';
+    const newMessage = {id: nanoid(12), author: 'currentUser', content: message, time}
+    console.log('Your Message : ',newMessage);
+    setState([...state, newMessage])
+  }
+  
+  return (
+    <div>
+      <nav className={styles.ThemeHeader}>
+        <Button size='small' onClick={handleGoBack}>Back</Button>
+        <h2>{topic.title}</h2>
+      </nav>
+      
+      <div className={styles.MsgContainer}>
+
+      {state.map((message) => (
+        
+        <div className={styles.Msg} key={message.id}>
+          <div className={styles.MsgHeader}>
+            <img className={styles.Ava} />
+            <span className={styles.Author}>{message.author} </span>
+            <span className={styles.Time}>{message.time}</span>
+          </div>
+          <div className={styles.MsgBody}>{message.content}</div>
+        </div>
+      ))}
+      </div>
+
+      <form className={styles.FormSendMsg} onSubmit={handleSubmit}>
+        <input className={styles.InputSendMsg}  name='message' placeholder='YOUR MESSAGE' autoComplete='off'/>
+        <Button size='small' type='submit'>SEND</Button>
+        </form>
+    </div>
+  );
+};
+
