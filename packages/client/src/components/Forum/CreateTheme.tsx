0c8b92aadcd86abecf3c3026a0f9fe353f2ec28f
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-05-05 12:13:12 +0300

    add react-hook-form

--- packages/client/src/components/Forum/CreateTheme.tsx
+++ packages/client/src/components/Forum/CreateTheme.tsx
@@ -1,27 +1,28 @@
 import * as React from 'react';
-import {
-  useNavigate
-} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { nanoid } from 'nanoid'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import styles from './CreateTheme.module.scss'
 import Button from '../UI/Button';
 import { Topic } from './Forum';
 
 
 export const CreateTheme: React.FC = () => {
+  //navigation
   const navigate = useNavigate();
   function handleGoBack() {
     navigate(-1);
   }
 
-  function handleSubmit(event) {
-  event.preventDefault(); 
-  const themeTitle: HTMLInputElement = event.target.elements.title;
-  const title: string = themeTitle.value;
-  const newTopic: Topic = {id: nanoid(10), title, messagesCount: 0, messages: []}
-  //topics.push(newTopic)
-  themeTitle.value = ''
-  console.log('New Topic : ',newTopic);
+  //form logic
+  type Input = {
+    title: string
+  }
+  const { register, handleSubmit, reset } = useForm<Input>()
+  const onSubmit: SubmitHandler<Input> = data => {
+    const topic: Topic = {...data, id: nanoid(10), messagesCount: 0, messages: []}
+    console.log('Topic data : ', topic)
+    reset({ title: '' })
   }
 
   return (
@@ -30,8 +31,8 @@ export const CreateTheme: React.FC = () => {
         <Button size='small' onClick={handleGoBack}>Back</Button>
         <h2>topic.title</h2>
       </div>
-      <form className={styles.FormSendMsg} onSubmit={handleSubmit}>
-        <input className={styles.InputSendMsg} name='title' placeholder='NEW TITLE THEME' autoComplete='off'/>
+      <form className={styles.FormSendMsg} onSubmit={handleSubmit(onSubmit)}>
+        <input className={styles.InputSendMsg} placeholder='NEW TITLE THEME' autoComplete='off' {...register('title', {required: true})} />
         <Button size='small' >CREATE</Button>
         </form>
       </div>
