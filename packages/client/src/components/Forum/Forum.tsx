984761029d2de48c582387b4fe131bc7f2cd8b47
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-05-05 12:26:19 +0300

    Formated, some fixes

--- packages/client/src/components/Forum/Forum.tsx
+++ packages/client/src/components/Forum/Forum.tsx
@@ -1,47 +1,66 @@
-import * as React from 'react';
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
-import styles from './Forum.module.scss';
+import * as React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import styles from './Forum.module.scss'
 import { ForumList } from './ForumList'
 import { ForumPage } from './ForumPage'
 import { ThemePage } from './ThemePage'
 import { CreateTheme } from './CreateTheme'
 
 export type Forum = {
-  id: string;
-  title: string;
-  topics: Topic[];
-  topicsCount: number;
-  messagesCount: number;
-};
+  id: string
+  title: string
+  topics: Topic[]
+  topicsCount: number
+  messagesCount: number
+}
 
 export type Topic = {
-  id: string;
-  title: string;
-  messagesCount: number;
-  messages: Message[];
-};
+  id: string
+  title: string
+  messagesCount: number
+  messages: Message[]
+}
 
 export type Message = {
-  id: string;
-  author: string;
-  content: string;
-  time: string; //Todo
-};
+  id: string
+  author: string
+  content: string
+  time: string //Todo
+}
 
 export const messages: Message[] = [
-  { id: '1', author: 'el nina', content: 'Сообщение 1 adowniawodnawodn donawdnoawndawo dnoawdnwoadnowadn NNAOIWNDOAIWNDIOANWDOAWNDOAWN', time: '23:20' },
-  { id: '2', author: 'el kampot', content: 'Сообщение 2 This is spartaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', time: '23:30' },
+  {
+    id: '1',
+    author: 'el nina',
+    content:
+      'Сообщение 1 adowniawodnawodn donawdnoawndawo dnoawdnwoadnowadn NNAOIWNDOAIWNDIOANWDOAWNDOAWN',
+    time: '23:20',
+  },
+  {
+    id: '2',
+    author: 'el kampot',
+    content:
+      'Сообщение 2 This is spartaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    time: '23:30',
+  },
   { id: '3', author: 'el bobo', content: 'Сообщение 3', time: '21:30' },
   { id: '4', author: 'el lol', content: 'Сообщение 4', time: '13:30' },
-];
+]
 
 export const topics: Topic[] = [
-  { id: '1', title: 'topic first 1111', messagesCount: 132, messages: messages },
-  { id: '2', title: 'topic second 2222', messagesCount: 121, messages: messages },
-];
+  {
+    id: '1',
+    title: 'topic first 1111',
+    messagesCount: 132,
+    messages: messages,
+  },
+  {
+    id: '2',
+    title: 'topic second 2222',
+    messagesCount: 121,
+    messages: messages,
+  },
+]
 export const forums: Forum[] = [
   {
     id: '1',
@@ -64,7 +83,7 @@ export const forums: Forum[] = [
     topicsCount: 5,
     messagesCount: 112,
   },
-];
+]
 export const Forum: React.FC = () => {
   return (
     <div className={styles.ForumContainer}>
@@ -75,7 +94,5 @@ export const Forum: React.FC = () => {
         <Route path="/:id/:id" element={<ThemePage />} />
       </Routes>
     </div>
-  );
-};
-
-
+  )
+}
