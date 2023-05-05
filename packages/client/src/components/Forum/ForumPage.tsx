e4b76711c35d8225083bdeaea8fdde7745090c5f
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-05-05 12:26:19 +0300

    Formated, some fixes

--- packages/client/src/components/Forum/ForumPage.tsx
+++ packages/client/src/components/Forum/ForumPage.tsx
@@ -1,52 +1,58 @@
-import * as React from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import * as React from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import styles from './ForumPage.module.scss'
 import { forums, topics } from './Forum'
-import Button from '../UI/Button';
-import { Topic, Forum } from './Forum';
+import Button from '../UI/Button'
+import { Topic, Forum } from './Forum'
 
 export const ForumPage: React.FC = () => {
-  const navigate = useNavigate();
- 
+  const navigate = useNavigate()
+
   function handleGoBack() {
-    navigate('/forum');
+    navigate('/forum')
   }
 
-  function handleGoCreate(){
+  function handleGoCreate() {
     navigate('/forum/newtheme')
   }
-  
-  const { id } = useParams<{ id: string }>();
-  const forum: Forum | undefined = forums.find((f) => f.id === id);
+
+  const { id } = useParams<{ id: string }>()
+  const forum: Forum | undefined = forums.find(f => f.id === id)
 
   if (!forum) {
     // если форум с таким id не найден, отображаем страницу ошибки
-    return <div>Форум не найден</div>;
+    return <div>Форум не найден</div>
   }
 
   return (
     <div>
       <nav className={styles.TopicHeader}>
-        <Button size='small' onClick={handleGoBack}>Back</Button>
+        <Button size="small" onClick={handleGoBack}>
+          Back
+        </Button>
         <h2>{forum.title}</h2>
-        <Button size='small' onClick={handleGoCreate}>CREATE THEME</Button>
+        <Button size="small" onClick={handleGoCreate}>
+          CREATE THEME
+        </Button>
       </nav>
       <ul className={styles.ListContainer}>
-      <li className={styles.ListElement}>
-            <div className={styles.HeaderContainer}>
-              <div className={styles.TitleHeader}>THEME</div>
-              <div className={styles.MsgCountHeader}>COMMENTS</div>
-            </div>
-          </li>
+        <li className={styles.ListElement}>
+          <div className={styles.HeaderContainer}>
+            <div className={styles.TitleHeader}>THEME</div>
+            <div className={styles.MsgCountHeader}>COMMENTS</div>
+          </div>
+        </li>
         {topics.map((topic: Topic) => (
           <li className={styles.ListElement} key={topic.id}>
-            <Link className={styles.LinkElement}  to={`/forum/${forum.id}/${topic.id}`}>
-            <div className={styles.Title}>{topic.title}</div>
-                <div className={styles.MsgCount}>{topic.messagesCount}</div>
-              </Link>
+            <Link
+              className={styles.LinkElement}
+              to={`/forum/${forum.id}/${topic.id}`}>
+              <div className={styles.Title}>{topic.title}</div>
+              <div className={styles.MsgCount}>{topic.messagesCount}</div>
+            </Link>
           </li>
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
--- packages/client/src/components/Forum/ThemePage.tsx
+++ packages/client/src/components/Forum/ThemePage.tsx
@@ -1,41 +1,36 @@
-import * as React from 'react';
-import {
-  useNavigate,
-  useParams,
-  useLocation,
-} from 'react-router-dom';
+import * as React from 'react'
+import { useNavigate, useParams, useLocation } from 'react-router-dom'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { nanoid } from 'nanoid'
-import styles from './ThemePage.module.scss';
-// import ava from '../../assets/ava.png'
+import styles from './ThemePage.module.scss'
 import { messages, topics, Message } from './Forum'
-import Button from '../UI/Button';
+import Button from '../UI/Button'
 
 export const ThemePage: React.FC = () => {
   //navigation
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
 
   function removePathSuffix(path: string): string {
-    const index = path.lastIndexOf('/');
+    const index = path.lastIndexOf('/')
     if (index >= 0) {
-      return path.substring(0, index);
+      return path.substring(0, index)
     } else {
-      return path;
+      return path
     }
   }
 
   function handleGoBack() {
-    navigate(removePathSuffix(pathname));
+    navigate(removePathSuffix(pathname))
   }
 
   //find current topic
-  const { id } = useParams<{ id: string }>();
-  const topic = topics.find((f) => f.id === id);
+  const { id } = useParams<{ id: string }>()
+  const topic = topics.find(f => f.id === id)
 
   if (!topic) {
     // если форум с таким id не найден, отображаем страницу ошибки
-    return <div>Тема не найден</div>;
+    return <div>Тема не найден</div>
   }
 
   //state
@@ -48,40 +43,53 @@ export const ThemePage: React.FC = () => {
   const { register, handleSubmit, reset } = useForm<Input>()
   const onSubmit: SubmitHandler<Input> = data => {
     const now = new Date()
-    const hours = now.getHours().toString();
-    const minutes = now.getMinutes().toString();
-    const time = `${hours}:${minutes}`;
-    const message = {...data, id: nanoid(12), author: 'currentUser', time}
+    const hours = now.getHours().toString()
+    const minutes = now.getMinutes().toString()
+    const time = `${hours}:${minutes}`
+    const message: Message = {
+      ...data,
+      id: nanoid(12),
+      author: 'currentUser',
+      time,
+    }
     console.log('Message data : ', message)
     setState([...state, message])
     reset({ content: '' })
   }
-  
+
   return (
     <div>
       <nav className={styles.ThemeHeader}>
-        <Button size='small' onClick={handleGoBack}>Back</Button>
+        <Button size="small" onClick={handleGoBack}>
+          Back
+        </Button>
         <h2>{topic.title}</h2>
       </nav>
-      
+
       <div className={styles.MsgContainer}>
-      {state.map((message) => (
-        <div className={styles.Msg} key={message.id}>
-          <div className={styles.MsgHeader}>
-            <img className={styles.Ava} />
-            <span className={styles.Author}>{message.author} </span>
-            <span className={styles.Time}>{message.time}</span>
+        {state.map(message => (
+          <div className={styles.Msg} key={message.id}>
+            <div className={styles.MsgHeader}>
+              <img className={styles.Ava} />
+              <span className={styles.Author}>{message.author} </span>
+              <span className={styles.Time}>{message.time}</span>
+            </div>
+            <div className={styles.MsgBody}>{message.content}</div>
           </div>
-          <div className={styles.MsgBody}>{message.content}</div>
-        </div>
-      ))}
+        ))}
       </div>
 
       <form className={styles.FormSendMsg} onSubmit={handleSubmit(onSubmit)}>
-        <input className={styles.InputSendMsg} placeholder='YOUR MESSAGE' autoComplete='off' {...register('content', { required: true })} />
-        <Button size='small' type='submit'>SEND</Button>
-        </form>
+        <input
+          className={styles.InputSendMsg}
+          placeholder="YOUR MESSAGE"
+          autoComplete="off"
+          {...register('content', { required: true })}
+        />
+        <Button size="small" type="submit">
+          SEND
+        </Button>
+      </form>
     </div>
-  );
-};
-
+  )
+}
