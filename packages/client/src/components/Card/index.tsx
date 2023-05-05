513884c448fd16410226f57f83ea12f0206ed4d2
rainstr7 <rainstr7@gmail.com>
2023-05-05 11:37:01 +0300

    refactoring: change component name and add static file for some time

--- packages/client/src/components/Card/index.tsx
+++ packages/client/src/components/Card/index.tsx
@@ -1,5 +1,5 @@
-import { UserName } from '../UserName'
 import cn from './style.module.scss'
+import Avatar from '../Avatar'
 
 interface CardProps {
   info: { label: string; value: number }[]
@@ -8,15 +8,11 @@ interface CardProps {
 const Card = ({ info }: CardProps) => {
   return (
     <div className={cn.Card}>
-      <UserName
-        name="KEKS OUALNIY"
-        image="https://sun9-62.userapi.com/s/v1/if2/hX2drELI1dlQ9an7NbVcl9inBZ4UXS9V2aUkS9FmB3C7STg2CRMY8THQursyE4tTuxJvx2cNmCI_YW3zrdlLLwh8.jpg?size=320x400&quality=96&type=album"
-      />
-
+      <Avatar name="KEKS OUALNIY" image="/avatar.jpeg" />
       <div className={''}>
         <ul>
           {info.map(({ label, value }) => (
-            <li>{`${label}: ${value}`}</li>
+            <li key={label}>{`${label}: ${value}`}</li>
           ))}
         </ul>
       </div>
