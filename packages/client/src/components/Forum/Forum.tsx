4c9a743345c970cf719af122133540a3b3d30932
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-05-07 17:31:59 +0300

    fix

--- packages/client/src/components/Forum/Forum.tsx
+++ packages/client/src/components/Forum/Forum.tsx
@@ -6,7 +6,7 @@ import ThemePage from './ThemePage'
 import CreateTheme from './CreateTheme'
 
 export type Forum = {
-  id: string
+  id: number
   title: string
   topics: Topic[]
   topicsCount: number
@@ -14,14 +14,14 @@ export type Forum = {
 }
 
 export type Topic = {
-  id: string
+  id: number
   title: string
   messagesCount: number
   messages: Message[]
 }
 
 export type Message = {
-  id: string
+  id: number
   author: string
   content: string
   time: string
@@ -29,32 +29,32 @@ export type Message = {
 
 export const messages: Message[] = [
   {
-    id: '1',
+    id: 1,
     author: 'el nina',
     content:
       'Сообщение 1 adowniawodnawodn donawdnoawndawo dnoawdnwoadnowadn NNAOIWNDOAIWNDIOANWDOAWNDOAWN',
     time: '23:20',
   },
   {
-    id: '2',
+    id: 2,
     author: 'el kampot',
     content:
       'Сообщение 2 This is spartaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
     time: '23:30',
   },
-  { id: '3', author: 'el bobo', content: 'Сообщение 3', time: '21:30' },
-  { id: '4', author: 'el lol', content: 'Сообщение 4', time: '13:30' },
+  { id: 3, author: 'el bobo', content: 'Сообщение 3', time: '21:30' },
+  { id: 4, author: 'el lol', content: 'Сообщение 4', time: '13:30' },
 ]
 
 export const topics: Topic[] = [
   {
-    id: '1',
+    id: 1,
     title: 'topic first 1111',
     messagesCount: 132,
     messages: messages,
   },
   {
-    id: '2',
+    id: 2,
     title: 'topic second 2222',
     messagesCount: 121,
     messages: messages,
@@ -62,21 +62,21 @@ export const topics: Topic[] = [
 ]
 export const forums: Forum[] = [
   {
-    id: '1',
+    id: 1,
     title: 'Forum 1',
     topics: topics,
     topicsCount: 4,
     messagesCount: 112,
   },
   {
-    id: '2',
+    id: 2,
     title: 'Forum 2',
     topics: topics,
     topicsCount: 3,
     messagesCount: 112,
   },
   {
-    id: '3',
+    id: 3,
     title: 'Forum 3',
     topics: topics,
     topicsCount: 5,
