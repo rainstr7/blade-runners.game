24fde9bde5ff6a34acd424c1579310f0e0c9b845
OvalniyKeks <keksovalniy@gmail.com>
2023-05-07 16:05:13 +0300

    pull dev and fix

--- packages/client/src/views/Profile/index.tsx
+++ packages/client/src/views/Profile/index.tsx
@@ -3,12 +3,16 @@ import cn from './style.module.scss'
 import Button from '../../components/UI/Button'
 import Input from '../../components/UI/Input'
 import CardLink from '../../components/UI/CardLink'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
 const ProfileFormData = [
   {
     placeholder: 'FIRST NAME',
     name: 'firstName',
     autoComplete: 'firstName',
+    required: true
   },
   {
     placeholder: 'SECOND NAME',
@@ -19,11 +23,13 @@ const ProfileFormData = [
     placeholder: 'E-MAIL',
     name: 'email',
     autoComplete: 'email',
+    required: true
   },
   {
     placeholder: 'PHONE',
     name: 'phone',
     autoComplete: 'phone',
+    required: true
   },
   {
     placeholder: 'LOGIN',
@@ -55,25 +61,37 @@ const initialState = {
   password: ''
 }
 
+const phoneRegExp = /^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    secondName: yup.string(),
+    email: yup.string().email().required(),
+    phone: yup
+      .string()
+      .required()
+      .matches(phoneRegExp, 'Phone number is not valid'),
+    login: yup.string(),
+    password: yup.string()
+  })
+  .required()
+
 const Profile = () => {
-  const [profileData, setprofileData] = useState<ProfileDataInterface>(initialState)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    resolver: yupResolver(schema),
+  })
 
   const handleExit = () => {
     console.log('exit')
   }
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = event => {
-    if (event.currentTarget) {
-      setprofileData({
-        ...profileData,
-        [event.currentTarget.name]: event.currentTarget.value,
-      })
-    }
-  }
-
-  const handleSubmit: FormEventHandler<HTMLFormElement> = event => {
-    event.preventDefault()
-    console.log('body', JSON.stringify(profileData))
+  const onSubmit: SubmitHandler<FieldValues> = data => {
+    console.log('body', JSON.stringify(data))
   }
 
   return (
@@ -83,18 +101,20 @@ const Profile = () => {
           <Button size='small' onClick={handleExit}>Exit</Button>
         </div>
         <div className={cn.formWrapper}>
-          <form className={cn.form} onSubmit={handleSubmit}>
-            {ProfileFormData.map(({ placeholder, name, autoComplete }) => (
+          <form className={cn.form} onSubmit={handleSubmit(onSubmit)}>
+            {ProfileFormData.map(({ placeholder, name, autoComplete, required }) => (
               <Input
                 placeholder={placeholder}
-                value={profileData[name as keyof typeof profileData]}
-                onChange={handleChange}
                 name={name}
                 autoComplete={autoComplete}
                 key={name}
+                register={register}
+                options={{ required }}
+                error={errors[name]}
               />
             ))}
             <div className={cn.formSubmit}><Button type='submit'>Save</Button></div>
+            {errors.login && <span>This field is required</span>}
           </form>
         </div>
       </main>
