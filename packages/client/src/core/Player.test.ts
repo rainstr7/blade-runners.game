2c39ad2729c2d24e925d8b796a55db79982d91a2
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-05-23 10:02:40 +0300

    update imports

--- packages/client/src/core/Player.test.ts
+++ packages/client/src/core/Player.test.ts
@@ -1,4 +1,5 @@
-import Player, { PlayerParams } from './player'
+import Player from './player'
+import { PlayerParams } from './types'
 
 describe('Player', () => {
   const gameWidth = 800
@@ -6,6 +7,7 @@ describe('Player', () => {
   const height = 50
   const width = 50
   const weight = 0.5
+  const imageSrc = '../assets/hero_run.png'
 
   const params: PlayerParams = {
     gameWidth,
@@ -13,6 +15,7 @@ describe('Player', () => {
     height,
     width,
     weight,
+    imageSrc,
   }
 
   let player: Player
@@ -24,15 +27,14 @@ describe('Player', () => {
   test('инициализация параметров игрока', () => {
     expect(player.x).toBe(150)
     expect(player.y).toBe(gameHeight - height)
-    expect(player.xV).toBe(0)
-    expect(player.yV).toBe(0)
+    expect(player.yV).toBe(20)
     expect(player.height).toBe(height)
     expect(player.width).toBe(width)
   })
 
   test('позиционирование игрока при нажатии на Пробел', () => {
     const pressedKeyCodes = ['Space']
-    player.update(pressedKeyCodes)
+    player.update(pressedKeyCodes, 1)
 
     expect(player.yV).toBe(-20 + weight)
     expect(player.y).toBe(gameHeight - height - 20)
@@ -42,17 +44,9 @@ describe('Player', () => {
     player.y = gameHeight - height - 10
     player.yV = 5
 
-    player.update([])
+    player.update([], 1)
 
     expect(player.yV).toBeGreaterThan(5)
     expect(player.y).toBeGreaterThan(gameHeight - height - 10)
-  })
-
-  test('если не нажать Пробел, позиция не должна изменяться', () => {
-    const pressedKeyCodes: string[] = []
-    player.update(pressedKeyCodes)
-
-    expect(player.yV).toBe(0)
-    expect(player.y).toBe(gameHeight - height)
   })
 })
