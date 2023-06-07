57222e8b60f2521236f4269b4f7d206b9ed4a74f
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-06-07 23:43:06 +0300

    update

--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import App from './src/App'
+import App from './src/App-ssr'
 
 export function render() {
   return renderToString(<App />)
