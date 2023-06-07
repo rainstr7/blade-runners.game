8e34c63f36c500b25d86b5972d25955a26deb5e8
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-06-07 20:39:17 +0300

    add SSR

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,7 +5,8 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
