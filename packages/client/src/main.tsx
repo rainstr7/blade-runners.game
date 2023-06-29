24a4527ef8326a99eba80aac4f68cb84b9abfac6
rainstr7 <rainstr7@gmail.com>
2023-06-29 12:34:19 +0300

    off ssr

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,18 +1,35 @@
+// import { StrictMode } from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App'
+// import { Provider } from 'react-redux'
+// import { initialServiceWorker } from '../sw/initialServiceWorker'
+// import { create } from './utils/createStore'
+//
+// initialServiceWorker()
+//
+// const store = create(window.__PRELOADED_STATE__)
+// delete window.__PRELOADED_STATE__
+//
+// ReactDOM.hydrateRoot(
+//   document.getElementById('root') as HTMLElement,
+//   <StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </StrictMode>
+// )
+
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { Provider } from 'react-redux'
-import { initialServiceWorker } from '../sw/initialServiceWorker'
-import { create } from './utils/createStore'
+import store from './store'
 
-initialServiceWorker()
-
-const store = create(window.__PRELOADED_STATE__)
-delete window.__PRELOADED_STATE__
-
-ReactDOM.hydrateRoot(
-  document.getElementById('root') as HTMLElement,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
