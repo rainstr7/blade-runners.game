56e9fd3f4dc7aee9b1be3bd94c55a137de1c4e4a
rainstr7 <rainstr7@gmail.com>
2023-06-29 12:55:37 +0300

    fixed vite config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-// import { VitePWA } from 'vite-plugin-pwa'
+ import { VitePWA } from 'vite-plugin-pwa'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    // VitePWA({ injectRegister: 'auto' }),
+    VitePWA({ injectRegister: 'auto' }),
     viteStaticCopy({
       targets: [
         {
