2502e00e32c5522041c10a80f35f912601085cb0
Aleksei Zagorskii <m1nerall@yandex.ru>
2023-07-04 01:17:37 +0300

    fix

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -27,9 +27,8 @@ import {
 const routes = ['/', '/signin', '/signup']
 
 async function startServer() {
-  const app = express()
-  app.use(cors())
-  const port = Number(process.env.SERVER_PORT) || 3001
+  dbConnect().then(async () => {
+    console.log('NOW DB IS GAD')
 
   // Подключаемся к БД
   dbConnect().then(async () => {
@@ -47,9 +46,11 @@ async function startServer() {
   //     console.error('Неудалось подключиться к БД: ', err)
   //   })
 
-  // sequelize.sync({ force: true })
+    let vite: ViteDevServer | undefined
 
-  let vite: ViteDevServer | undefined
+    const distPath = path.resolve('../../client/dist')
+    const srcPath = path.resolve('../../client')
+    const ssrClientPath = path.resolve('../../client/ssr-dist/client.cjs')
 
   const distPath = path.resolve('../../client/dist')
   const srcPath = path.resolve('../../client')
@@ -143,36 +144,105 @@ async function startServer() {
         loading: { loading: false },
       })
 
-      let renderUrl = '/'
-      const availRoute = routes.find(r => r === url)
-      if (availRoute) {
-        renderUrl = availRoute
-      }
+      app.use(vite.middlewares)
+    }
 
-      const appHtml = await render(store, renderUrl)
+    app.get('/api', (_, res) => {
+      res.json('👋 Howdy from the server :)')
+    })
 
-      const html = template.replace(
-        `<!--ssr-outlet-->`,
-        appHtml +
-          `<script> 
+    if (!isDev()) {
+      app.use('/assets', express.static(path.resolve(distPath, 'assets')))
+    }
+
+    app.use('*', async (req, res, next) => {
+      const url = req.originalUrl
+
+      try {
+        let template: string
+
+        if (!isDev()) {
+          template = fs.readFileSync(
+            path.resolve(distPath, 'index.html'),
+            'utf-8'
+          )
+        } else {
+          template = fs.readFileSync(
+            path.resolve(srcPath, 'index.html'),
+            'utf-8'
+          )
+
+          template = await vite!.transformIndexHtml(url, template)
+        }
+
+        let create: (initialState: any) => any
+        let render: (store: any, url: string) => Promise<string>
+
+        if (!isDev()) {
+          render = (await import(ssrClientPath)).render
+          create = (await import(ssrClientPath)).create
+        } else {
+          render = (await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx')))
+            .render
+
+          create = (await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx')))
+            .create
+        }
+
+        const store = create({
+          score: {
+            value: 0,
+            leaderboard: [],
+          },
+          user: {
+            id: undefined,
+            first_name: undefined,
+            second_name: undefined,
+            display_name: undefined,
+            login: undefined,
+            email: undefined,
+            phone: undefined,
+            avatar: undefined,
+          },
+          alert: {
+            show: false,
+            type: 'success',
+            text: '',
+          },
+          loading: { loading: false },
+        })
+
+        let renderUrl = '/'
+        const availRoute = routes.find(r => r === url)
+        if (availRoute) {
+          renderUrl = availRoute
+        }
+
+        const appHtml = await render(store, renderUrl)
+
+        const html = template.replace(
+          `<!--ssr-outlet-->`,
+          appHtml +
+            `<script> 
           window.__PRELOADED_STATE__=${JSON.stringify(store.getState()).replace(
             /</g,
             '\\u003c'
           )}
         </script>`
-      )
+        )
 
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
-    } catch (e) {
-      if (isDev()) {
-        vite!.ssrFixStacktrace(e as Error)
+        res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+      } catch (e) {
+        if (isDev()) {
+          vite!.ssrFixStacktrace(e as Error)
+        }
+        next(e)
       }
-      next(e)
-    }
-  })
+    })
 
-  app.listen(port, () => {
-    console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+    app.listen(port, () => {
+      console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+    })
   })
 }
 
