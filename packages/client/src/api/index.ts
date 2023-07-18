56016d2e829291b758f5764a92464f53dd235c66
rainstr7 <rainstr7@gmail.com>
2023-07-18 10:37:21 +0300

    change redirect

--- packages/client/src/api/index.ts
+++ packages/client/src/api/index.ts
@@ -1,9 +1,8 @@
-// import { REDIRECT_URI } from '../config/oAuth.config'
+import { REDIRECT_URI } from '../config/oAuth.config'
 
 export const BASE_API = 'https://ya-praktikum.tech/api/v2'
-export const SERVER_API = `http://localhost:3001/api`
-// export const SERVER_API = `${REDIRECT_URI}api` todo поменять перед деплоем на облако
-
+// export const SERVER_API = `http://localhost:3001/api`
+export const SERVER_API = `${REDIRECT_URI}/api`
 export const signin = `${BASE_API}/auth/signin`
 export const oAuthSignin = `${BASE_API}/oauth/yandex`
 export const signup = `${BASE_API}/auth/signup`
